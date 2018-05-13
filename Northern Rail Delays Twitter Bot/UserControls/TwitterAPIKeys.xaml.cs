5e701c497562e2b785a729da4d89cd3f4bba389a
Callum Houghton <callum.houghton13@hotmail.co.uk>
2018-05-14 00:16:31 +0100

    Changed some string messages, and DB values.

---
+++ Northern Rail Delays Twitter Bot/UserControls/TwitterAPIKeys.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Northern_Rail_Delays_Twitter_Bot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Northern_Rail_Delays_Twitter_Bot.UserControls
+{
+    /// <summary>
+    /// Interaction logic for TwitterApIKeys.xaml
+    /// </summary>
+    public partial class TwitterAPIKeysWindow : Window
+    {
+        Database db = new Database();
+        Dictionary<string, string> twitterAPIKeys = new Dictionary<string, string>();
+        public static RichTextBox mainOutputTxtBox;
+        public TwitterAPIKeysWindow()
+        {
+            InitializeComponent();
+            SetTextBoxToTwitAPIKeys();
+        }
+
+        private void SetTextBoxToTwitAPIKeys()
+        {
+            twitterAPIKeys = db.GetTwitterAPIKeys();
+            CusKey.Text = twitterAPIKeys["cusKey"];
+            CusKeySecret.Text = twitterAPIKeys["cusKeySecret"];
+            AssTok.Text = twitterAPIKeys["assTok"];
+            AssTokSecret.Text = twitterAPIKeys["assTokSecret"];
+
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (CusKey.Text != "" && CusKeySecret.Text !="" && AssTok.Text != "" && AssTokSecret.Text != "")
+            {
+                twitterAPIKeys.Clear();
+                twitterAPIKeys.Add("cusKey", CusKey.Text);
+                twitterAPIKeys.Add("cusKeySecret", CusKeySecret.Text);
+                twitterAPIKeys.Add("assTok", AssTok.Text);
+                twitterAPIKeys.Add("assTokSecret", AssTokSecret.Text);
+                db.SetTwitterAPIKeys(twitterAPIKeys);
+                mainOutputTxtBox.AppendText("\rTwitter API keys have been successfully updated.");
+                this.Close();
+            }
+
+            else
+            {
+                MessageBox.Show("One of the API key text boxes does not have a value!", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+    }
+}
